c56cf49e6ed21b067e73d9af90015d4fe3f299e7
PradeepKopp <koppolipradeep@gmail.com>
2024-06-23 20:23:05 +1000

    Testing the log

--- setup.py
+++ setup.py
@@ -1,37 +1,36 @@
 from setuptools import setup
 from typing import List
 
+# Declaring variables for setup functions
+PROJECT_NAME = "Housing-Predictor"
+VERSION = "0.0.1"
+AUTHOR = "Pradeep"
+DESCRIPTION = "This is my first Machine Learning project Pipeline"
+PACKAGES = ["housing"]
+REQUIREMENT_FILE_NAME = "requirements.txt"
 
-
-
-
-#Declaring variables for setup functions
-PROJECT_NAME="Housing-Predictor"
-VERSION="0.0.1"
-AUTHOR="Pradeep"
-DESCRIPTION="This is my first Machine Learning project Pipeline"
-PACKAGES=["housing"]
-REQUIREMENT_FILE_NAME="requirements.txt"
-
-def get_requirements_list()->List[str]:
+def get_requirements_list() -> List[str]:
     """
-    Description:This function is going to return list of requirement
-    mention in requirements.txt file
-    return this function is going to return a list 
-    which contain name of libraries mentioned in requirements.txt file
+    Description: This function is going to return list of requirements
+    mentioned in requirements.txt file.
+    return: This function is going to return a list 
+    which contains the names of libraries mentioned in requirements.txt file.
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-       return requirement_file.readlines().remove("-e .")
-
+        requirements = requirement_file.readlines()
+        requirements = [requirement.strip() for requirement in requirements]
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+        return requirements
 
 setup(
-name=PROJECT_NAME,
-version=VERSION,
-author=AUTHOR,
-description=DESCRIPTION,
-packages=PACKAGES,
-install_requires=get_requirements_list()
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
 )
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print(get_requirements_list())
